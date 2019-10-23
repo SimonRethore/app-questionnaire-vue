feccf888300f180a45c592a33e82f54674a8da13
SimonRethore <srethore.pro@gmail.com>
2019-10-23 11:11:30 +0200

    Input + Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,43 @@
-
-// <style lang="scss">
-// #app {
-//   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-//   -webkit-font-smoothing: antialiased;
-//   -moz-osx-font-smoothing: grayscale;
-//   text-align: center;
-//   color: #2c3e50;
-// }
-
-// #nav {
-//   padding: 30px;
-
-//   a {
-//     font-weight: bold;
-//     color: #2c3e50;
-
-//     &.router-link-exact-active {
-//       color: #42b983;
-//     }
-//   }
-// }
-
-// .input{
-//   width: 100%;
-//   height: 100px;
-// }
-// </style>
-
 <template>
   <div id="app">
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
+
+    <b-navbar  toggleable="sm" class="main-navbar" type="dark" variant="dark" >
+      <b-navbar-toggle class="button-navbar" target="nav-text-collapse"></b-navbar-toggle>
+
+      <b-navbar-brand class="libelle-navbar">EEV SAS</b-navbar-brand>
+
+      <b-collapse id="nav-text-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-text class="text-navbar mx-4">Faire le test</b-nav-text>
+          <b-nav-text class="text-navbar mx-4">Administration</b-nav-text>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+
     <router-view/>
   </div>
 </template>
+
+<style>
+  .main-navbar{
+    background-color: #494ca2;
+  }
+
+  .button-navbar{
+    /* color: white !important; */
+    border-color: white !important;
+  }
+
+  .libelle-navbar{
+    color: white !important;
+  }
+
+  .text-navbar{
+    color: white !important;
+  }
+
+  .navbar-collapse{
+    right: 0;
+    position: absolute;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,25 @@
 <template>
   <div>
-    <div class="container align-middle">
-      <div class="row">
-        <div class="col-10 mx-auto">
-          <b-form-input class="text-center my-3" v-model="text" placeholder="Prénom"></b-form-input>
-          <b-form-input class="text-center my-3" v-model="text" placeholder="Nom"></b-form-input>
-          <b-form-input class="text-center my-3" v-model="text" placeholder="Société"></b-form-input>
+    <div class="container-fluid">
+      <div class="row h-100">
+        <div class="col-10 m-auto">
+          <b-form-input class="text-center my-3" placeholder="Prénom"></b-form-input>
+          <b-form-input class="text-center my-3" placeholder="Nom"></b-form-input>
+          <b-form-input class="text-center my-3" placeholder="Société"></b-form-input>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<style>
+  .container-fluid{
+    position: fixed;
+    height: 100%;
+    bottom: 0;
+    width: 100%;
+  }
+</style>
 
 <script>
 // @ is an alias to /src
