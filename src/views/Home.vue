a0aa870350f22028b3fba211b965f4808a03d9a6
SimonRethore <srethore.pro@gmail.com>
2019-10-23 09:56:31 +0200

    Initialisation

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="container align-middle">
+      <div class="row">
+        <div class="col-10 mx-auto">
+          <b-form-input class="text-center my-3" v-model="text" placeholder="Prénom"></b-form-input>
+          <b-form-input class="text-center my-3" v-model="text" placeholder="Nom"></b-form-input>
+          <b-form-input class="text-center my-3" v-model="text" placeholder="Société"></b-form-input>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home'
+  // components: {
+  // }
+}
+</script>
